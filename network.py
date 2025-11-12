55e520dd9a0215a3976836ad0fd0b8480bb392b5
Jai Singh Pannu <dev.jaipannu@gmail.com>
2025-11-12 01:32:11 -0500

    Installed dependencies needed to build the CNN

--- network.py
+++ network.py
@@ -2,3 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+from sklearn.datasets import fetch_openml
+mnist = fetch_openml(name="mnist_784")
+
+print(mnist.keys())
+
+dict_keys = {'data', 'target', 'frame', 'categories', 'feature_names', 'target_names', 'DESCR', 'details', 'url'}
+
+data = mnist.data
+labels = mnist.target
+
+n = np.random.choice(np.arrange(data.shape[0]+1))
+
+print(n)
